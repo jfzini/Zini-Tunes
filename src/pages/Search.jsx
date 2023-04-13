490e56a431c485f3668eb176b001444ca5948803
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-13 19:38:49 -0300

    req 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    searchQuery: '',
+    isDisabled: false,
+  };
+
+  validateQuery = (value) => {
+    const minLength = 2;
+    this.setState({
+      isDisabled: value.length >= minLength,
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchQuery: value,
+    }, () => this.validateQuery(value));
+  };
+
   render() {
+    const { isDisabled, searchQuery } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <h2>Search</h2>
+        <form action="">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search-artist"
+            placeholder="Nome do artista"
+            onChange={ this.handleChange }
+            value={ searchQuery }
+          />
+          <button data-testid="search-artist-button" disabled={ !isDisabled }>
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
