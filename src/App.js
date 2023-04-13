2f93a48ddea1cf31e4e32c45050cdf5d730f878f
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-13 15:13:43 -0300

    wip: req 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Favorites from './pages/Favorites';
 import Album from './pages/Album';
@@ -12,13 +12,15 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route exact path="*" component={ NotFound } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    nameValue: '',
+    validName: false,
+  };
+
+  validateName = (value) => {
+    const minLength = 3;
+    this.setState({
+      validName: value.length >= minLength,
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      nameValue: value,
+    }, () => this.validateName(value));
+  };
+
   render() {
+    const { nameValue, validName } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <h2>Login</h2>
+        <form action="">
+          <label htmlFor="user-name">Nome:</label>
+          <input
+            type="text"
+            id="user-name"
+            data-testid="login-name-input"
+            value={ nameValue }
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="login-submit-button"
+            disabled={ !validName }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
