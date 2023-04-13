f969732b97b64072b5981f7e55c6f42de5f08838
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-13 14:53:45 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Favorites from './pages/Favorites';
+import Album from './pages/Album';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes </p>);
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
