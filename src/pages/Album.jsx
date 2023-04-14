8a6634d76ddc07b3ae5638b75556d8c0358b33d9
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-14 17:15:55 -0300

    req 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -2,34 +2,54 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    artist: '',
+    albumName: '',
+    slicedAlbumData: [],
+  };
+
   async componentDidMount() {
     const {
       match: {
         params: { id },
       },
     } = this.props;
-    const test = await getMusics(id);
-    console.log(test);
+    const albumData = await getMusics(id);
+    const artist = albumData[0].artistName;
+    const albumName = albumData[0].collectionName;
+    const slicedAlbumData = albumData.slice(1);
+    this.setState({
+      artist,
+      albumName,
+      slicedAlbumData,
+    });
   }
 
   render() {
-
-    const {
-      location: {
-        state: {
-          name,
-        },
-      },
-    } = this.props;
+    const { artist, albumName, slicedAlbumData } = this.state;
 
     return (
       <div data-testid="page-album">
         <Header />
-        <p>
-          {name ? `Álbum de ${name}` : ""}
+        <p data-testid="album-name">{albumName ? `${albumName}` : ''}</p>
+        <p data-testid="artist-name">
+          {artist ? `${artist}` : ''}
         </p>
+        <div>
+          {slicedAlbumData
+            ? slicedAlbumData
+              .map(({ trackName, previewUrl, trackId }) => (
+                <MusicCard
+                  key={ trackId }
+                  trackName={ trackName }
+                  previewUrl={ previewUrl }
+                />
+              ))
+            : ''}
+        </div>
       </div>
     );
   }
