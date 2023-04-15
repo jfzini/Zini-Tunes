0c600ba1e6fb39c2d66d2cfb985d41240f42ba1d
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-15 16:59:35 -0300

    req 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
-
   state = {
     isLoading: false,
-    name: "",
+    name: '',
     email: '',
     image: '',
     description: '',
@@ -25,7 +25,7 @@ class ProfileEdit extends Component {
     this.setState({
       isLoading: true,
     });
-    const {name, email, image, description} = await getUser();
+    const { name, email, image, description } = await getUser();
     this.setState({
       isLoading: false,
       name,
@@ -35,45 +35,52 @@ class ProfileEdit extends Component {
     });
   };
 
-  handleChange = ({target}) => {
+  handleChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: value,
-    })
+    });
     this.validateTextInputs(target);
-  }
+  };
 
   validateEmail = (value) => {
     const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     this.setState({
       emailValid: regex.test(value),
-    })
-  }
+    });
+  };
 
   validateTextInputs = (target) => {
     const { value, name, type } = target;
     if (type === 'email') {
-      this.validateEmail(value)
-      return
+      this.validateEmail(value);
+      return;
     }
-    const inputName = `${name}Valid` 
+    const inputName = `${name}Valid`;
     this.setState({
       [inputName]: value.length > 0,
-    })
-  }
+    });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, email, image, description } = this.state;
+    const { history } = this.props;
+    await updateUser({ name, email, image, description });
+    history.push('/profile');
+  };
 
   render() {
-    
     const { isLoading,
-            name,
-            email,
-            image,
-            description,
-            nameValid,
-            emailValid,
-            descriptionValid,
-            imageValid 
+      name,
+      email,
+      image,
+      description,
+      nameValid,
+      emailValid,
+      descriptionValid,
+      imageValid,
     } = this.state;
 
     const fieldsValid = nameValid && emailValid && descriptionValid && imageValid;
@@ -85,38 +92,43 @@ class ProfileEdit extends Component {
           ? (<Loading />)
           : (
             <form>
+              <p>Editar perfil</p>
               <input
                 type="text"
-                placeholder='Nome'
+                placeholder="Nome"
                 data-testid="edit-input-name"
-                name='name'
-                value={name}
-                onChange={this.handleChange}
+                name="name"
+                value={ name }
+                onChange={ this.handleChange }
               />
               <input
                 type="email"
                 name="email"
                 id=""
-                placeholder='Email'
+                placeholder="Email"
                 data-testid="edit-input-email"
-                value={email}
-                onChange={this.handleChange}
+                value={ email }
+                onChange={ this.handleChange }
               />
               <textarea
                 name="description"
                 id=""
                 data-testid="edit-input-description"
-                value={description}
-                onChange={this.handleChange}
+                value={ description }
+                onChange={ this.handleChange }
               />
               <input
                 type="text"
-                value={image}
-                name='image'
+                value={ image }
+                name="image"
                 data-testid="edit-input-image"
-                onChange={this.handleChange}
+                onChange={ this.handleChange }
               />
-              <button data-testid="edit-button-save" disabled={!fieldsValid}>
+              <button
+                data-testid="edit-button-save"
+                disabled={ !fieldsValid }
+                onClick={ this.handleSubmit }
+              >
                 Salvar alterações
               </button>
             </form>)}
@@ -126,3 +138,9 @@ class ProfileEdit extends Component {
 }
 
 export default ProfileEdit;
+
+ProfileEdit.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
